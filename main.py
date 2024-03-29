4d859c82ebf88398d7c5e93c46d207594fd1ca7a
RafaelTaves <rafaeljosetaves@gmail.com>
2024-03-29 00:41:02 -0300

    att boas praticas

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+# Importações
 import sqlite3
 import json
 
+
+# Definições de funções
+
+
+def zerar_tabela():
+    comando_limpar_tabela = "DELETE FROM clientes"
+    cursor.execute(comando_limpar_tabela)
+
+
+def inserir_dado_generico():
+    cursor.execute("INSERT INTO clientes VALUES ('Rafael', '400289922', 'rafael@gmail.com', '20765057760')")
+    conexao.commit()
+    cursor.execute("INSERT INTO clientes VALUES ('Carol', '200489922', 'carol@gmail.com', '20045057760')")
+    conexao.commit()
+    cursor.execute("INSERT INTO clientes VALUES ('Miguel', '200589922', 'rafael@gmail.com', '20055057760')")
+    conexao.commit()
+
+
 # Conectando ao banco SQLite
 conexao = sqlite3.connect('banco.db')
-
 # Definindo objeto cursor para executar comando em SQL através do python
 cursor = conexao.cursor()
 
-# Zerando tabela
-comandoLimparTabela = "DELETE FROM clientes"
-cursor.execute(comandoLimparTabela)
+# Limpando tabela e inserindo dados genéricos
+zerar_tabela()
 
-# Inserindo valores genéricos na tabela
-cursor.execute("INSERT INTO clientes VALUES ('Rafael', '400289922', 'rafael@gmail.com', '20765057760')")
-conexao.commit()
-cursor.execute("INSERT INTO clientes VALUES ('Carol', '200489922', 'carol@gmail.com', '20045057760')")
-conexao.commit()
-cursor.execute("INSERT INTO clientes VALUES ('Miguel', '200589922', 'rafael@gmail.com', '20055057760')")
-conexao.commit()
+inserir_dado_generico()
 
 # Selecionando a primeira tupla da tabela
 cursor.execute("SELECT * FROM clientes LIMIT 1")
